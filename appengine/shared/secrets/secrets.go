dafe17b664008203c3dfc21f9c071b8d7e509f68
Leigh McCulloch <leigh@mcchouse.com>
2017-11-20 06:24:26 +0000

    Add caching of secrets

--- appengine/shared/secrets/secrets.go
+++ appengine/shared/secrets/secrets.go
@@ -8,6 +8,8 @@ import (
 
 	"cloud.google.com/go/storage"
 	"google.golang.org/appengine/file"
+	"google.golang.org/appengine/log"
+	"google.golang.org/appengine/memcache"
 )
 
 // Get returns the secret for the given name.
@@ -20,6 +22,15 @@ func Get(ctx context.Context, name string) string {
 }
 
 func get(ctx context.Context, name string) (string, error) {
+	objectName := path.Join("secrets", name)
+	cacheName := path.Join("storage", objectName)
+
+	if item, err := memcache.Get(ctx, cacheName); err == nil {
+		return string(item.Value), nil
+	} else if err != memcache.ErrCacheMiss {
+		log.Errorf(ctx, "failed to get secret from memcache: %v", err)
+	}
+
 	bucket, err := file.DefaultBucketName(ctx)
 	if err != nil {
 		return "", fmt.Errorf("failed to get default bucket: %v", err)
@@ -29,8 +40,6 @@ func get(ctx context.Context, name string) (string, error) {
 	if err != nil {
 		return "", fmt.Errorf("failed to create storage client: %v", err)
 	}
-
-	objectName := path.Join("secrets", name)
 
 	r, err := client.Bucket(bucket).Object(objectName).NewReader(ctx)
 	if err != nil {
@@ -42,5 +51,10 @@ func get(ctx context.Context, name string) (string, error) {
 	if err != nil {
 		return "", fmt.Errorf("failed to read bucket %s file %s: %v", bucket, objectName, err)
 	}
+
+	if err := memcache.Set(ctx, &memcache.Item{Key: cacheName, Value: []byte(data)}); err != nil {
+		log.Errorf(ctx, "failed to set secret in memcache: %v", err)
+	}
+
 	return string(data), nil
 }
