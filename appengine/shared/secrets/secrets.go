857cdb8c118588660a3b2359391805509d5a38fa
Leigh McCulloch <leigh@mcchouse.com>
2017-11-20 07:09:19 +0000

    Move secrets to datastore

--- appengine/shared/secrets/secrets.go
+++ appengine/shared/secrets/secrets.go
@@ -3,11 +3,9 @@ package secrets
 import (
 	"context"
 	"fmt"
-	"io/ioutil"
 	"path"
 
-	"cloud.google.com/go/storage"
-	"google.golang.org/appengine/file"
+	"google.golang.org/appengine/datastore"
 	"google.golang.org/appengine/log"
 	"google.golang.org/appengine/memcache"
 )
@@ -22,39 +20,24 @@ func Get(ctx context.Context, name string) string {
 }
 
 func get(ctx context.Context, name string) (string, error) {
-	objectName := path.Join("secrets", name)
-	cacheName := path.Join("storage", objectName)
-
+	cacheName := path.Join("datastore", "secret", name)
 	if item, err := memcache.Get(ctx, cacheName); err == nil {
 		return string(item.Value), nil
 	} else if err != memcache.ErrCacheMiss {
 		log.Errorf(ctx, "failed to get secret from memcache: %v", err)
 	}
 
-	bucket, err := file.DefaultBucketName(ctx)
-	if err != nil {
-		return "", fmt.Errorf("failed to get default bucket: %v", err)
+	key := datastore.NewKey(ctx, "secret", name, 0, nil)
+	secret := struct {
+		Value string `datastore:"value"`
+	}{}
+	if err := datastore.Get(ctx, key, &secret); err != nil {
+		return "", fmt.Errorf("failed to get secret from datastore %s: %v", name, err)
 	}
 
-	client, err := storage.NewClient(ctx)
-	if err != nil {
-		return "", fmt.Errorf("failed to create storage client: %v", err)
-	}
-
-	r, err := client.Bucket(bucket).Object(objectName).NewReader(ctx)
-	if err != nil {
-		return "", fmt.Errorf("failed to read bucket %s file %s: %v", bucket, objectName, err)
-	}
-	defer r.Close()
-
-	data, err := ioutil.ReadAll(r)
-	if err != nil {
-		return "", fmt.Errorf("failed to read bucket %s file %s: %v", bucket, objectName, err)
-	}
-
-	if err := memcache.Set(ctx, &memcache.Item{Key: cacheName, Value: []byte(data)}); err != nil {
+	if err := memcache.Set(ctx, &memcache.Item{Key: cacheName, Value: []byte(secret.Value)}); err != nil {
 		log.Errorf(ctx, "failed to set secret in memcache: %v", err)
 	}
 
-	return string(data), nil
+	return secret.Value, nil
 }
